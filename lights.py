a66678708168b426ce2b10d62ee1b31ccad945c3
karl <karl.bittner@hexaquo.at>
2020-11-04 14:45:36 +0100

    Finished int is printed
    Next: actually send to tradfri

--- lights.py
+++ lights.py
@@ -5,6 +5,7 @@ import time
 temperature_targets = {}
 pause_time = 5
 
+# Parse the temperature target CSV file
 with open('temperatures.csv') as temperatures_csv:
     reader = csv.reader(temperatures_csv, delimiter=',')
 
@@ -20,8 +21,6 @@ with open('temperatures.csv') as temperatures_csv:
     
     num_entries = len(lines)
     
-    print(lines)
-    
     for i in range(0, num_entries):
         current_time = datetime.datetime.strptime(lines[i][0], "%H:%M").time()
         current_temp = int(lines[i][1])
@@ -32,9 +31,17 @@ with open('temperatures.csv') as temperatures_csv:
         
         temperature_targets[(current_time, target_time)] = (current_temp, target_temp)
 
+# Convert a time like 12:15 to 12.25.
 def time_to_float(t):
-    return t.hour + t.minute / 60.0
+    return t.hour + t.minute / 60.0 + t.second / 3600.0
 
+# We alternate between value and value+1, because there is an issue
+#  with a light being turned off, getting a new value, turned on,
+#  and getting the same value again
+#  (https://github.com/home-assistant-libs/pytradfri/issues/270)
+alternator = 0
+
+# Main Loop
 while(True):
     current_time = datetime.datetime.now().time()
 
@@ -48,6 +55,7 @@ while(True):
             t = (floatime - start) / (target - start)
             interpolated = temp_begin + (temp_end - temp_begin) * t
             
-            print(interpolated)
+            print(int(interpolated) + alternator)
     
+    alternator = (alternator + 1) % 2
     time.sleep(pause_time)
