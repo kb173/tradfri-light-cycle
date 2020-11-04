f8505328b794602ccb5ba8a491fa34a35e0a97ea
karl <karl.bittner@hexaquo.at>
2020-11-04 14:06:08 +0100

    Parse and continuously print interpolated times

--- lights.py
+++ lights.py
@@ -1,26 +1,53 @@
-from datetime import datetime
-from datetime import time
+import datetime
+import csv
+import time
 
-temperature_targets = {
-    (time(5, 00), time(20, 00)): (250, 250),
-    (time(20, 00), time(21, 00)): (250, 300),
-    (time(21, 00), time(00, 00)): (300, 400),
-    (time(00, 00), time(2, 00)): (400, 450)
-}
+temperature_targets = {}
+pause_time = 5
+
+with open('temperatures.csv') as temperatures_csv:
+    reader = csv.reader(temperatures_csv, delimiter=',')
+
+    lines = []
+    
+    for row in reader:
+        row_to_add = []
+        
+        for col in reader:
+            row_to_add.append(col)
+        
+        lines += row_to_add
+    
+    num_entries = len(lines)
+    
+    print(lines)
+    
+    for i in range(0, num_entries):
+        current_time = datetime.datetime.strptime(lines[i][0], "%H:%M").time()
+        current_temp = int(lines[i][1])
+        
+        next_i = (i + 1) % num_entries
+        target_time = datetime.datetime.strptime(lines[next_i][0], "%H:%M").time()
+        target_temp = int(lines[next_i][1])
+        
+        temperature_targets[(current_time, target_time)] = (current_temp, target_temp)
 
 def time_to_float(t):
     return t.hour + t.minute / 60.0
 
-current_time = datetime.now().time()
+while(True):
+    current_time = datetime.datetime.now().time()
 
-for (begin, end), (temp_begin, temp_end) in temperature_targets.items():
-    if current_time > begin and current_time < end:
-        # Interpolate
-        floatime = time_to_float(current_time)
-        start = time_to_float(begin)
-        target = time_to_float(end)
-        
-        t = (floatime - start) / (target - start)
-        interpolated = temp_begin + (temp_end - temp_begin) * t
-        
-        print(interpolated)
+    for (begin, end), (temp_begin, temp_end) in temperature_targets.items():
+        if current_time > begin and current_time < end:
+            # Interpolate
+            floatime = time_to_float(current_time)
+            start = time_to_float(begin)
+            target = time_to_float(end)
+            
+            t = (floatime - start) / (target - start)
+            interpolated = temp_begin + (temp_end - temp_begin) * t
+            
+            print(interpolated)
+    
+    time.sleep(pause_time)
